732cb0c63bb49c063e4c7122cb7edc3afa4f801a
Birat Poudel <poudel.birat25@gmail.com>
2022-02-02 18:51:54 +0545

    feedback section

--- javascript/script.js
+++ javascript/script.js
@@ -51,3 +51,38 @@ $(document).ready(function () {
         time: 1200,
     });
 });
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        margin: 15,
+        autoplay: true,
+        autoplayhoverpause: true,
+        autoplaytimeout: 100,
+        items: 3,
+        nav: true,
+        loop: true,
+        stagePadding: 0,
+        responsive: {
+            0: {
+                items: 1.08,
+                dots: true
+            },
+            375: {
+                items: 1.08,
+                dots: true
+            },
+            425: {
+                items: 1.38,
+                dots: true
+            },
+            768: {
+                items: 2.38,
+                dots: true
+            },
+            1200: {
+                items: 3.05,
+                dots: true
+            },
+        }
+    });
+})
