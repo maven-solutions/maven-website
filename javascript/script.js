ca368f47fd37936e42941613be30ccecd4e55731
Abid Adhikari <abidadhikari@gmail.com>
2022-02-28 12:49:04 +0545

    slick carousel direction fixed , contact from  theme changed, services box seperated

--- javascript/script.js
+++ javascript/script.js
@@ -79,6 +79,7 @@ $('.st__body').slick({
     autoplay: true,
     dots: false,
     nextArrow: false,
+    // rtl:true,
     responsive: [
         {
             breakpoint: 965,
@@ -111,12 +112,13 @@ $('.st__body1').slick({
     speed: 300,
     dots: true,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll:1,
     autoplay: false,
     dots: false,
     prevArrow: false,
-    autoplay: false,
+    autoplay: true,
     dots: false,
+    // rtl:false,
     responsive: [
         {
             breakpoint: 965,
@@ -142,3 +144,120 @@ $('.st__body1').slick({
         },
     ]
 });
+
+
+let autoplaySpeed = 2500;
+let autoplayOn    = true;
+
+// let $slickRoot = ;
+
+$('.st__body').on('init', function() {
+    let $slickList = $('.st__body').find('.slick-list');
+
+    $slickList.mouseenter(function() {
+        autoplayOn = false;
+    });
+    $slickList.mouseleave(function() {
+        autoplayOn = true;
+    });
+
+    window.setInterval(function() {
+        if (!autoplayOn) return;
+        $('.st__body').slick('slickPrev');
+    }, autoplaySpeed);
+});
+
+// $('.st__body1').on('init', function() {
+//     let $slickList = $('.st__body1').find('.slick-list');
+
+//     $slickList.mouseenter(function() {
+//         autoplayOn = false;
+//     });
+//     $slickList.mouseleave(function() {
+//         autoplayOn = true;
+//     });
+
+//     window.setInterval(function() {
+//         if (!autoplayOn) return;
+//         $('.st__body1').slick('slickPrev');
+//     }, autoplaySpeed);
+// });
+
+
+$('.st__body').slick({
+    adaptiveHeight: false,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll:1,
+    autoplay: false,
+    dots: false,
+    prevArrow: false,
+    autoplay: true,
+    dots: false,
+    // rtl:false,
+    responsive: [
+        {
+            breakpoint: 965,
+            settings: {
+                slidesToShow: 3,
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 744,
+            settings: {
+                slidesToShow: 2,
+                arrows: true
+
+            }
+        },
+        {
+            breakpoint: 410,
+            settings: {
+                slidesToShow: 1,
+                arrows: false
+            }
+        },
+    ]
+});
+
+// $('.st__body1').slick({
+//     adaptiveHeight: false,
+//     infinite: true,
+//     speed: 300,
+//     dots: true,
+//     slidesToShow: 4,
+//     slidesToScroll:1,
+//     autoplay: false,
+//     dots: false,
+//     prevArrow: false,
+//     autoplay: true,
+//     dots: false,
+//     // rtl:false,
+//     responsive: [
+//         {
+//             breakpoint: 965,
+//             settings: {
+//                 slidesToShow: 3,
+//                 arrows: true
+//             }
+//         },
+//         {
+//             breakpoint: 744,
+//             settings: {
+//                 slidesToShow: 2,
+//                 arrows: true
+
+//             }
+//         },
+//         {
+//             breakpoint: 410,
+//             settings: {
+//                 slidesToShow: 1,
+//                 arrows: false
+//             }
+//         },
+//     ]
+// });
